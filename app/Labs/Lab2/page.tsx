2fd5d881862ea47df2c6424223e4263fbbc5900b
Shraavya B K <Shraavya.kishan@gmail.com>
2025-09-15 02:30:06 -0700

    Buttons

---
+++ app/Labs/Lab2/page.tsx
@@ -0,0 +1,8 @@
+export default function Lab2() {
+  return (
+    <div>
+      <h2>Lab 2</h2>
+      <h2>Intro to CSS</h2>
+    </div>
+  );
+}
---
+++ app/Labs/Lab3/page.tsx
@@ -0,0 +1,8 @@
+export default function Lab3() {
+  return (
+    <div>
+      <h2>Lab 3</h2>
+      <h2>Intro to TypeScript</h2>
+    </div>
+  );
+}
