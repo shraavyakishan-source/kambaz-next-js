c767712fe8049a3e5a6c5901f1f4063b19d75385
Shraavya B K <Shraavya.kishan@gmail.com>
2025-09-24 16:40:12 -0700

    pages error

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -129,8 +129,8 @@ export default function Lab1() {
       <br />
       <textarea id="wd-textarea" cols={30} rows={10}>
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
         essentially unchanged. It was popularised in the 1960s with the release
