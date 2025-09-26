f21571493d6d9b7951c78f9147197492ea001469
Shraavya B K <Shraavya.kishan@gmail.com>
2025-09-26 01:10:50 -0700

    lab 2 error fixes

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -31,7 +31,7 @@ export default function Lab2() {
         <h3>ID selectors</h3>
         <p id="wd-id-selector-1">
           Instead of changing the look and feel of all the elements of the same
-          name, e.g., P, we can refer to a specific element by its ID
+          name, e.g. P, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
           Here is another paragraph using a different ID and a different look
