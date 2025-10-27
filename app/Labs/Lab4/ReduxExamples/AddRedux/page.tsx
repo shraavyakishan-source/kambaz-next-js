faade0eee57f0e7ffac9d761ec4508295a37ab17
Shraavya B K <Shraavya.kishan@gmail.com>
2025-10-27 02:28:37 -0700

    ERROR

--- app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
@@ -1,53 +1,41 @@
 "use client";
 import { useSelector, useDispatch } from "react-redux";
 import type { AppDispatch } from "../../store";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { add } from "./addReducer";
 import { Button, FormControl } from "react-bootstrap";
 import React from "react";
-import { Provider } from "react-redux";
-import store from "../../store";
 
 export default function AddRedux() {
   const [a, setA] = useState(12);
   const [b, setB] = useState(23);
-  const [isClient, setIsClient] = useState(false);
 
   const { sum } = useSelector(
     (state: { addReducer: { sum: number } }) => state.addReducer
   );
   const dispatch = useDispatch<AppDispatch>();
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return <div>Loading...</div>;
-  }
   return (
-    <Provider store={store}>
-      <div className="w-25" id="wd-add-redux">
-        <h2>Passing Data to Reducers</h2>
-        <h2>Add Redux</h2>
-        <h2>
-          {a} + {b} = {sum}
-        </h2>
-        <FormControl
-          type="number"
-          defaultValue={a}
-          onChange={(e) => setA(parseInt(e.target.value) || 0)}
-        />
-        <FormControl
-          type="number"
-          defaultValue={b}
-          onChange={(e) => setB(parseInt(e.target.value) || 0)}
-        />
-        <Button id="wd-add-redux-click" onClick={() => dispatch(add({ a, b }))}>
-          Add Redux
-        </Button>
-        <hr />
-      </div>
-    </Provider>
+    <div className="w-25" id="wd-add-redux">
+      <h2>Passing Data to Reducers</h2>
+      <h2>Add Redux</h2>
+      <h2>
+        {a} + {b} = {sum}
+      </h2>
+      <FormControl
+        type="number"
+        value={a}
+        onChange={(e) => setA(parseInt(e.target.value) || 0)}
+      />
+      <FormControl
+        type="number"
+        value={b}
+        onChange={(e) => setB(parseInt(e.target.value) || 0)}
+      />
+      <Button id="wd-add-redux-click" onClick={() => dispatch(add({ a, b }))}>
+        Add Redux
+      </Button>
+      <hr />
+    </div>
   );
 }
--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,19 +1,24 @@
+"use client";
 import { ReactNode } from "react";
 import TOC from "./TOC";
-
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./Lab4/store";
 export default function LabsLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td valign="top" width="100px">
-            <TOC />
-          </td>
-          <td valign="top">{children}</td>
-        </tr>
-      </tbody>
-    </table>
+    <Provider store={store}>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="100px">
+              <TOC />
+            </td>
+            <td valign="top">{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </Provider>
   );
 }
