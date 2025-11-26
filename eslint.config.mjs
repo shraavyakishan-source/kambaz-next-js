1ea4aff83fdd6910e7c98642d8704bfa5f3faad4
Shraavya B K <Shraavya.kishan@gmail.com>
2025-11-26 02:20:11 -0800

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // Disable only 'any' type warnings
+      "@typescript-eslint/no-explicit-any": "off",
+    },
   },
 ];
 
